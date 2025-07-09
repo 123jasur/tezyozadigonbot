9fe3118dee6181b336a1b3cb0ecee286e06eff1d
jasurbek <ajoyibuzb954@gmail.com>
2025-07-09 15:04:31 +0500

    Update auto.py

--- auto.py
+++ auto.py
@@ -206,15 +206,4 @@ def run():
 
 t = Thread(target=run)
 t.start()
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 4000
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
 
